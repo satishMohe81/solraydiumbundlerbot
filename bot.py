b452242c91c6633ddd576cda32bac25d5ae63f0a
satishMohe81 <imsatish811@gmail.com>
2025-05-15 23:47:37 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,12 @@ import os
 from telegram import Update
 from telegram.ext import Updater, CommandHandler
 
-def start(update: Update, context):
-    update.message.reply_text("✅ Bot is LIVE!")
-
 TOKEN = os.getenv("TELEGRAM_TOKEN")
-if not TOKEN:
-    raise ValueError("❌ Missing Telegram Token")
+
+def start(update: Update, context):
+    update.message.reply_text("🤖 Bot is working!")
 
 updater = Updater(TOKEN)
 updater.dispatcher.add_handler(CommandHandler("start", start))
-print("🚀 Starting bot...")
+print("🚀 Bot started")
 updater.start_polling()
