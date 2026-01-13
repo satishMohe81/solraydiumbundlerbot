7b8de07c261a434c8c70c89001c850b93d6b8be5
satishMohe81 <imsatish811@gmail.com>
2026-01-13 11:30:54 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,11 @@ TOKEN = os.getenv("TELEGRAM_TOKEN")
 def start(update: Update, context):
     update.message.reply_text("🤖 Bot is working!")
 
-updater = Updater(TOKEN)
-updater.dispatcher.add_handler(CommandHandler("start", start))
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
+
+dp.add_handler(CommandHandler("start", start))
+
 print("🚀 Bot started")
 updater.start_polling()
+updater.idle()
